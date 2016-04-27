e4c13bfb603a91ef4cfc6661bd29c57c5e4bef8a
Amanda Robinson <amanda.robinson@gsa.gov>
2016-04-26 22:41:31 -0400

    Create Relying Party app

--- README.md
+++ README.md
@@ -2,4 +2,10 @@ Identity-rp
 
 Mock relying party app for validating IDP and IdV APIs.
 
-May also function as reference RP implementation, 
+May also function as reference RP implementation.
+
+# Testing
+`bundle exec ruby test/app_test.rb`
+
+# Running
+`bundle exec ruby app.rb`
---
+++ app.rb
@@ -0,0 +1,32 @@
+require 'erb'
+require 'net/http'
+require 'sinatra/base'
+
+class RelyingParty < Sinatra::Base
+  def init(uri)
+    @auth_server_uri = uri
+  end
+
+  def auth_server_uri
+    @auth_server_uri ||= URI('https://localhost:1234')
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/login/?' do
+    begin
+      Net::HTTP.post_form(auth_server_uri, params)
+    rescue
+      # Fake a success for now
+      call env.merge("PATH_INFO" => '/success', "REQUEST_METHOD" => 'GET')
+    end
+  end
+
+  get '/success/?' do
+    '<b>Success!</b>'
+  end
+
+  run! if app_file == $0
+end
