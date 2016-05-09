239ad3769b402ac67574c02883a807a03e27cd89
Brendan Sudol <brendan.sudol@gsa.gov>
2016-05-09 18:11:48 -0400

    add uscis simulated homepage

--- test/app_test.rb
+++ test/app_test.rb
@@ -18,6 +18,18 @@ class FakeIdvaasTest < Test::Unit::TestCase
     assert last_response.body.include? '<form action="/login" method="POST">'
   end
 
+  def test_agency_template_override
+    get '/?agency=uscis'
+    assert last_response.body.include? 'img/uscis/logo.png'
+    assert !(last_response.body.include? 'img/seal.png')
+  end
+
+  def test_agency_template_no_override
+    get '/?agency=foo'
+    assert !(last_response.body.include? 'img/uscis/logo.png')
+    assert last_response.body.include? 'img/seal.png'
+  end
+
   def test_it_shows_success
     get '/success/'
     assert last_response.ok?
