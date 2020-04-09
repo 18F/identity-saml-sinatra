e628e8c8b33b2fd7a7e976a2fbd2a90ef4512f25
Zach Margolis <zachary.margolis@gsa.gov>
2020-04-09 12:20:38 -0700

    Update to match OIDC sinatra with variable IAL level

--- app.rb
+++ app.rb
@@ -38,8 +38,18 @@ class RelyingParty < Sinatra::Base
       session[:agency] = agency
       erb :"agency/#{agency}/index", layout: false, locals: { logout_msg: logout_msg }
     else
+      ial = params[:ial] || '1'
+      ial1_link_class = 'text-underline' if ial == '1'
+      ial2_link_class = 'text-underline' if ial == '2'
+
       session.delete(:agency)
-      erb :index, locals: { logout_msg: logout_msg, login_msg: login_msg }
+      erb :index, locals: {
+        logout_msg: logout_msg,
+        login_msg: login_msg,
+        login_path: "/login_get?ial=#{ial}",
+        ial1_link_class: ial1_link_class,
+        ial2_link_class: ial2_link_class,
+      }
     end
   end
 
@@ -47,15 +57,18 @@ class RelyingParty < Sinatra::Base
     puts "Logging in via GET"
     request = OneLogin::RubySaml::Authrequest.new
     puts "Request: #{request}"
-    redirect to(request.create(saml_settings))
+    ial = params[:ial] || '1'
+    redirect to(request.create(saml_settings(ial: ial)))
   end
 
   post '/login_post/?' do
     puts "Logging in via POST"
     saml_request = OneLogin::RubySaml::Authrequest.new
     puts "Request: #{saml_request}"
-    post_params = saml_request.create_params(saml_settings, 'RelayState' => params[:id])
-    login_url   = saml_settings.idp_sso_target_url
+    ial = params[:ial] || '1'
+    settings = saml_settings(ial: ial)
+    post_params = saml_request.create_params(settings, 'RelayState' => params[:id])
+    login_url   = settings.idp_sso_target_url
     erb :login_post, locals: { login_url: login_url, post_params: post_params }
   end
 
@@ -125,9 +138,11 @@ class RelyingParty < Sinatra::Base
     end
   end
 
-  def saml_settings
+  def saml_settings(ial: nil)
     template = File.read('config/saml_settings.yml')
     base_config = Hashie::Mash.new(YAML.safe_load(ERB.new(template).result(binding)))
+
+    base_config.authn_context = "http://idmanagement.gov/ns/assurance/ial/#{ial}" if ial
 
     # TODO: don't use the demo cert and key in EC2 environments
     if LoginGov::Hostdata.in_datacenter? && (
